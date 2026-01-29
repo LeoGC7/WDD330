f266d31b98482de91b2f4bb7ec1c4de8096292af
LeoGC7 <lcorrea@byupathway.edu>
2026-01-29 12:43:47 -0700

    Dynamic Header and Footer added

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFotter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 const listing = new ProductList("tents", dataSource, listElement);
 
-console.log(listing);
+loadHeaderFotter()
 
 listing.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -34,9 +34,32 @@ export function getParam(param) {
 
 export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(templateFn);
-  // if clear is true we need to clear out the contents of the parent.
   if (clear) {
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if(callback) {
+    callback(data)
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template
+}
+
+export async function loadHeaderFotter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const headerElement = document.getElementById("main-header");
+  const footerElement = document.getElementById("main-footer")
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
