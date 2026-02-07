f9b2dfaa6189f400c03f3b6bfdb8c7ddb257da06
LeoGC7 <lcorrea@byupathway.edu>
2026-02-07 16:59:33 -0700

    cart quantities fixed (comments removed)

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,11 +2,9 @@ import ShoppingCart from "./ShoppingCart.mjs";
 import { getCartTotalValue } from "./utils.mjs";
 
 const cart = new ShoppingCart("so-cart", document.querySelector(".cart-list"));
-window.cart = cart; // This makes it work with the 'onclick' above
+window.cart = cart;
 cart.init();
 
-// Set Total
 document.getElementById("totalValue").innerHTML = `U$${getCartTotalValue().toFixed(2)}`;
 
-// Clear Cart button
 document.getElementById("cartButton").addEventListener("click", () => cart.clearCart());
