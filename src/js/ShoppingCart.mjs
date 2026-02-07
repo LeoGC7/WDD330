324d7cb9852e3a020170e7db9f812195167ec249
LeoGC7 <lcorrea@byupathway.edu>
2026-02-07 16:53:33 -0700

    cart quantity fixed

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+import { getLocalStorage, renderListWithTemplate, setLocalStorage } from "./utils.mjs";
 
 export default class ShoppingCart {
     constructor(key, listElement) {
@@ -13,7 +13,33 @@ export default class ShoppingCart {
     }
 
     renderList(list) {
-        renderListWithTemplate(cartItemTemplate, this.listElement, list, "afterbegin", true)
+    const groupedList = list.reduce((acc, item) => {
+      const existing = acc.find(i => i.Id === item.Id);
+      if (existing) {
+        existing.quantity += 1;
+      } else {
+        acc.push({ ...item, quantity: 1 });
+      }
+      return acc;
+    }, []);
+
+    renderListWithTemplate(cartItemTemplate, this.listElement, groupedList, "afterbegin", true);
+  }
+
+    addOne(id) {
+    let cart = getLocalStorage(this.key);
+    const item = cart.find(i => i.Id === id);
+    cart.push(item);
+    setLocalStorage(this.key, cart);
+    location.reload();
+  }
+
+    removeOne(id) {
+      let cart = getLocalStorage(this.key);
+      const index = cart.findIndex(i => i.Id === id);
+      if (index > -1) cart.splice(index, 1);
+      setLocalStorage(this.key, cart);
+      location.reload();
     }
 
     clearCart() {
@@ -23,18 +49,25 @@ export default class ShoppingCart {
 }
 
 function cartItemTemplate(item) {
-  return `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Images.PrimarySmall}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  return `
+  <li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Images.PrimarySmall}"
+        alt="${item.Name}"
+      />
+    </a>
+    <div class="cart-name-color">
+      <a href="#"><h2 class="card__name">${item.Name}</h2></a>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    </div>
+    <div class="cart-quantity-price">
+      <p class="cart-card__quantity">qty: ${item.quantity}</p>
+      <p class="cart-card__price">$${(item.FinalPrice * item.quantity).toFixed(2)}</p>
+    </div>
+    <div class="cart-item-buttons">
+      <button onclick="window.cart.addOne('${item.Id}')">+</button>
+      <button onclick="window.cart.removeOne('${item.Id}')">-</button>
+    </div>
+  </li>`;
 }
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,35 +1,12 @@
 import ShoppingCart from "./ShoppingCart.mjs";
 import { getCartTotalValue } from "./utils.mjs";
 
-// function cartItemTemplate(item) {
-//   const newItem = `<li class="cart-card divider">
-//   <a href="#" class="cart-card__image">
-//     <img
-//       src="${item.Image}"
-//       alt="${item.Name}"
-//     />
-//   </a>
-//   <a href="#">
-//     <h2 class="card__name">${item.Name}</h2>
-//   </a>
-//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-//   <p class="cart-card__quantity">qty: 1</p>
-//   <p class="cart-card__price">$${item.FinalPrice}</p>
-// </li>`;
-
-//   return newItem;
-// }
-
 const cart = new ShoppingCart("so-cart", document.querySelector(".cart-list"));
-
+window.cart = cart; // This makes it work with the 'onclick' above
 cart.init();
 
-const cartButton = document.getElementById("cartButton");
-cartButton.addEventListener("click", () => {
-  cart.clearCart();
-});
+// Set Total
+document.getElementById("totalValue").innerHTML = `U$${getCartTotalValue().toFixed(2)}`;
 
-const totalElement = document.getElementById("totalValue");
-const totalValue = getCartTotalValue();
-
-totalElement.innerHTML = `U$${totalValue}`
+// Clear Cart button
+document.getElementById("cartButton").addEventListener("click", () => cart.clearCart());
